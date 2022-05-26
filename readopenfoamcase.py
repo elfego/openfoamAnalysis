344267933e0e50b4264b768b8f274bb6ef73f538
Elfego Ruiz Gutierrez <elfegoruiz@gmail.com>
2022-05-26 16:49:24 +0100

    loading openfoam system properties

--- readopenfoamcase.py
+++ readopenfoamcase.py
@@ -1,29 +1,111 @@
 from os.path import join, exists
-from numpy import arange
-
-
-def list_time_dirs(case_dir):
-    sys_dir = join(case_dir, 'system')
-    with open(join(sys_dir, 'controlDict'), 'r') as handler:
-        for ln in handler.readlines():
-            ln2 = ln.split(' ')
-            if ln2[0] == 'startTime':
-                startTime = float(ln2[1][:-2])
-            if ln2[0] == 'endTime':
-                endTime = float(ln2[1][:-2])
-            if ln2[0] == 'writeInterval':
-                writeInterval = float(ln2[1][:-2])
-
-    frames = arange(startTime, endTime, writeInterval) + writeInterval
-    t_dirs = [f'{t:g}' for t in frames]
-
-    return t_dirs
+from numpy import arange, array
+import re
+import openfoamparser as Ofpp
 
 #
 
 
+class OFphase:
+    def __init__(self, name, transport_model, viscosity, density):
+        self.name = name
+        self.transport_model = transport_model
+        self.viscosity = viscosity
+        self.density = density
+        
+
 class readOFcase:
-    def __init__(self, case_dir):
+    def __init__(self, case_dir=None):
+        if not case_dir is None:
+            self.setup(case_dir)
+
+    def setup(self, case_dir):
         self.case_dir = case_dir
-        self.times = list_time_dirs(self.case_dir)
-        self.existing = list(map(self.times, exists))
+        self.system_dir = join(case_dir, 'system')
+        self.constant_dir = join(case_dir, 'constant')
+        self.times = self.list_time_dirs()
+        self.existing = list(map(lambda f: exists(join(case_dir, f)),
+                                 self.times))
+        self.app_name = self.get_application()
+        phases = self.get_phases()
+        self.surface_tension = self.get_surface_tension()
+        self.diffusivity = self.get_diffusivity()
+
+        self.transport_properties = {p: self.get_properties(p)
+                                     for p in phases}
+        self.g = self.get_gravity()
+        
+    def list_time_dirs(self):
+        with open(join(self.system_dir, 'controlDict'), 'r') as handler:
+            for ln in handler.readlines():
+                ln2 = ln.split(' ')
+                if ln2[0] == 'startTime':
+                    startTime = float(ln2[1][:-2])
+                if ln2[0] == 'endTime':
+                    endTime = float(ln2[1][:-2])
+                if ln2[0] == 'writeInterval':
+                    writeInterval = float(ln2[1][:-2])
+
+        frames = arange(startTime, endTime, writeInterval) + writeInterval
+        t_dirs = [f'{t:g}' for t in frames]
+
+        return t_dirs
+
+    def get_application(self):
+        with open(join(self.system_dir, 'controlDict'), 'r') as handler:
+            for ln in handler.readlines():
+                ln2 = ln.split(' ')
+                if ln2[0] == 'application':
+                    return ln2[1][:-2]
+            
+    def get_phases(self):
+        with open(join(self.constant_dir, 'transportProperties'), 'r') as handler:
+            for ln in handler.readlines():
+                ln2 = ln.split(' ')
+                if ln2[0] == 'phases':
+                    return ' '.join(ln2[1:]).split('(')[1].split(')')[0].split(' ')
+
+    def get_surface_tension(self):
+        with open(join(self.constant_dir, 'transportProperties'), 'r') as handler:
+            for ln in handler.readlines():
+                ln2 = ln.split(' ')
+
+                if ln2[0] == 'sigma12':
+                    result = float(' '.join(ln2[1:])[:-2])
+                if ln2[0] == 'sigma13':
+                    if result != float(' '.join(ln2[1:])[:-2]):
+                        print('Surface tensions are unequal!')
+                    return result
+            
+    def get_properties(self, phase):
+        with open(join(self.constant_dir, 'transportProperties'), 'r') as handler:
+            lns = ''.join(handler.readlines())
+            tmp = lns.split('phases')[1]
+
+            tmp, = re.findall(phase + '\s*\{[^\}]*\}', tmp)
+
+            transport_model = re.findall('transportModel\s*\w*', tmp)[0].split(' ')[-1]
+            viscosity = float(re.findall('nu\s*[^\;]*;', tmp)[0].split(' ')[-1][:-1])
+            density = float(re.findall('rho\s*[^\;]*;', tmp)[0].split(' ')[-1][:-1])
+
+        return {'transportModel': transport_model,
+                'viscosity': viscosity,
+                'density': density}
+
+    def get_diffusivity(self):
+        with open(join(self.constant_dir, 'transportProperties'), 'r') as handler:
+            lns = ''.join(handler.readlines())
+            D = float(re.findall('D23\s*[^\;]*;', lns)[0].split(' ')[-1][:-1])
+            return D
+
+    def get_gravity(self):
+        with open(join(self.constant_dir, 'g'), 'r') as handler:
+            lns = ''.join(handler.readlines())
+            tmp, = re.findall('value\s*\([^\(]*\)', lns)
+            tmp, = re.findall('\([^\)]*\)', tmp)
+            return array(list(map(float, tmp[1:-1].split(' '))))
+
+#
+
+rc = readOFcase('/DataB/mix_L2/base')
+print(rc.g)
