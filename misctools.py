152253388f49d243d5957c8b9bc4ecee0b8b4900
Elfego Ruiz Gutierrez <elfego.r.gutierrez@northumbria.ac.uk>
2022-05-31 22:41:45 +0100

    a better version

--- misctools.py
+++ misctools.py
@@ -1,5 +1,7 @@
-from numpy import sum, dot, save
-from numpy.linalg import det
+from numpy import sum, dot, save, nditer, zeros_like, hstack
+from numpy.linalg import det, eigh, norm
+import numba_scipy
+from numba import jit, prange, float64
 
 
 def calc_3rd_inv(A_flat):
@@ -15,9 +17,46 @@ def calc_val_weighted(X, dV, normalised=False, fsave=None):
     return result
 
 
-class OFphase:
-    def __init__(self, name, transport_model, viscosity, density):
-        self.name = name
-        self.transport_model = transport_model
-        self.viscosity = viscosity
-        self.density = density
+@jit(nopython=True, cache=True, fastmath=True, nogil=True)
+def get_vorticity(A):
+    return np.real(np.array([
+        A[2, 1] - A[1, 2],
+        A[0, 2] - A[2, 0],
+        A[1, 0] - A[0, 1]
+    ]))
+
+
+@jit(nopython=True, cache=True, fastmath=True, nogil=True)
+def enstrophy(A):
+    return 0.5 * ((A[2, 1] - A[1, 2])**2 +
+                  (A[0, 2] - A[2, 0])**2 +
+                  (A[1, 0] - A[0, 1])**2)
+
+
+@jit(nopython=True, cache=True, nogil=True, fastmath=True)
+def normalise(v):
+    n = norm(v)
+    if n < 1.0e-12:
+        return v / n
+    else:
+        return v
+
+
+def dSigma(alpha1, alpha2, gradAlpha1, gradAlpha2, V, out=None):
+    with nditer([alpha1, alpha2, gradAlpha1.T, gradAlpha2.T, V, out],
+                flags=['external_loop'] * 5 + ['buffered'],
+                op_flags=[['readonly']] * 5 + [['writeonly', 'allocate']]) as it:
+        for a1, a2, ga1, ga2, v, y in it:
+            y[...] = (a1 * ga2 - a2 * ga1) * v
+        return it.operands[5].T
+
+
+def local_eigensystem(gradU):
+    A = gradU.reshape(3, 3)
+    w, v = eigh(0.5 * (A + A.T))
+    idx = w.argsort()[::-1]
+
+    return hstack((normalise(v[:, idx[0]]),
+                   normalise(v[:, idx[1]]),
+                   normalise(v[:, idx[2]]),
+                   w[idx]))
--- readopenfoamcase.py
+++ readopenfoamcase.py
@@ -1,16 +1,11 @@
 from os.path import join, exists
 from os import makedirs
-from numpy import arange, array, r_, pi, cbrt, sum, save, load, sqrt, dot, zeros_like, savez_compressed
-from numpy.linalg import norm, det
+from numpy import (arange, array, r_, pi, cbrt, sum, save, savez_compressed,
+                   load, sqrt, dot, zeros, zeros_like)
+from numpy.linalg import norm, det, eigh
 import re
 import openfoamparser as Ofpp
-from misctools import calc_val_weighted, calc_3rd_inv
-
-#
-
-#
-
-#
+from misctools import calc_val_weighted, calc_3rd_inv, dSigma, normalise, local_eigensystem
 
 
 class readOFcase:
@@ -134,113 +129,6 @@ class readOFcase:
             tmp, = re.findall('\([^\)]*\)', tmp)
             return array(list(map(float, tmp[1:-1].split(' '))))
 
-    def calc_dimensinless_numbers(self, time):
-        t_dir = join(self.case_dir, time)
-        o_dir = join(self.out_dir, time)
-        makedirs(o_dir, exist_ok=True)
-
-        nu1 = self.transport_properties['pregel']['viscosity']
-        nu2 = self.transport_properties['crosslinker']['viscosity']
-
-        rho1 = self.transport_properties['pregel']['density']
-        rho2 = self.transport_properties['crosslinker']['density']
-
-        alpha1 = self.load_field('alpha.pregel', t_dir)
-        alpha2 = self.load_field('alpha.crosslinker', t_dir)
-        alpha3 = self.load_field('alpha.air', t_dir)
-        U      = self.load_field('U', t_dir)
-
-        if not self.mesh_loaded:
-            self.load_mesh()
-
-        dv1 = alpha1 * self.V
-        dv2 = alpha2 * self.V
-
-        V1 = sum(dv1)
-        save(join(o_dir,      'V.pregel.npy'), r_[V1, 2. * cbrt(0.75 * V1 / pi)])
-        V2 = sum(dv2)
-        save(join(o_dir, 'V.crosslinker.npy'), r_[V2, 2. * cbrt(0.75 * V2 / pi)])
-
-        X1 = calc_val_weighted(self.C, dv1,
-                               normalised=True,
-                               fsave=join(o_dir, 'X.pregel.npy'))
-        X2 = calc_val_weighted(self.C, dv2,
-                               normalised=True,
-                               fsave=join(o_dir, 'X.crosslinker.npy'))
-
-        U1 = calc_val_weighted(U, dv1,
-                               normalised=True,
-                               fsave=join(o_dir, 'U.pregel.npy'))
-        U2 = calc_val_weighted(U, dv2,
-                               normalised=True,
-                               fsave=join(o_dir, 'U.crosslinker.npy'))
-
-        Ur = norm(U2 - U1)
-
-        Ucm = (V1 * U1 + V2 * U2) / (V1 + V2)
-        save(join(o_dir, 'Ucm.npy'), Ucm)
-
-        b1 = norm(X2 - X1)
-        b2 = dot(U2 - U1, X2 - X1) / Ur
-        save(join(o_dir, 'distance.npy'), r_[b1, b2])
-        b = sqrt(b1 ** 2 - b2 ** 2)
-
-        B = 0.5 * b / self.Rnozzle
-        save(join(o_dir, 'impact_param.npy'), r_[b, B])
-
-        We_collision = (rho1 + rho2) * self.Rnozzle * Ur * Ur / self.surface_tension
-        Re_collision = 4 * self.Rnozzle * Ur / (nu1 + nu2)
-        save(join(o_dir, 'We_Re.npy'), r_[We_collision, Re_collision])
-        return None
-
-    def calc_mixture_measures(self, time):
-        t_dir = join(self.case_dir, time)
-        o_dir = join(self.out_dir, time)
-        makedirs(o_dir, exist_ok=True)
-
-        alpha1 = self.load_field('alpha.pregel', t_dir)
-        alpha2 = self.load_field('alpha.crosslinker', t_dir)
-        gradAlpha1 = self.load_field('grad(alpha.pregel)', t_dir)
-        gradAlpha2 = self.load_field('grad(alpha.crosslinker)', t_dir)
-
-        if not self.mesh_loaded:
-            self.load_mesh()
-
-        dv1 = alpha1 * self.V
-        dv2 = alpha2 * self.V
-
-        magGradAlpha1 = norm(gradAlpha1, axis=1)
-        magGradAlpha2 = norm(gradAlpha2, axis=1)
-
-        A_c = dot(magGradAlpha2, dv1) + dot(magGradAlpha1, dv2)
-        save(join(o_dir, 'surfaceDensityFunction.npy'), A_c)
-        V_m = 4 * dot(alpha1, dv2)
-        save(join(o_dir, 'mixtureVolume'), V_m)
-        E_mu = self.diffusivity * (dot(magGradAlpha2**2, dv1) +
-                                   dot(magGradAlpha1**2, dv2))
-        save(join(o_dir, 'scalarDissipationRate.npy'), E_mu)
-
-        return None
-
-    def calc_classification(self, time):
-        t_dir = join(self.case_dir, time)
-        o_dir = join(self.out_dir, time)
-        makedirs(o_dir, exist_ok=True)
-
-        Q = self.load_field('Q', t_dir)
-        R = self.load_post_field('3rd_invariant.npy', time)
-        n = 2 * (R > zeros_like(R)) + (4 * Q ** 3 + 27 * R ** 2 > zeros_like(Q))
-        savez_compressed(join(o_dir, 'classification.npz'), n)
-
-    def calc_R(self, time):
-        t_dir = join(self.case_dir, time)
-        o_dir = join(self.out_dir, time)
-        makedirs(o_dir, exist_ok=True)
-
-        gradU = self.load_field('grad(U)', t_dir)
-        R = array(list(map(calc_3rd_inv, gradU)))
-        save(join(o_dir, '3rd_invariant.npy'), R)
-
     def forAllTimes(self, func, *args, **kwargs):
         for e, t in zip(self.existing, self.times):
             if not e:
@@ -249,44 +137,307 @@ class readOFcase:
             print(t, ' : ', func.__name__, '...')
             func(t, *args, **kwargs)
 
-    def calc_visc_dissipation(self, time):
+    def calc_droplet_volumes(self, time):
         t_dir = join(self.case_dir, time)
         o_dir = join(self.out_dir, time)
         makedirs(o_dir, exist_ok=True)
 
+        if (exists(join(o_dir, 'V.pregel.npy')) and
+            exists(join(o_dir, 'V.crosslinker.npy'))):
+            return None
+
+        alpha1 = self.load_field('alpha.pregel', t_dir)
+        alpha2 = self.load_field('alpha.crosslinker', t_dir)
+
+        if not self.mesh_loaded:
+            self.load_mesh()
+
+        save(join(o_dir,      'V.pregel.npy'), dot(alpha1, self.V))
+        save(join(o_dir, 'V.crosslinker.npy'), dot(alpha2, self.V))
+
+    def calc_Xcm(self, time):
+        t_dir = join(self.case_dir, time)
+        o_dir = join(self.out_dir, time)
+        makedirs(o_dir, exist_ok=True)
+
+        if (exists(join(o_dir, 'X.pregel.npy')) and
+            exists(join(o_dir, 'X.crosslinker.npy'))):
+            return None
+
+        alpha1 = self.load_field('alpha.pregel', t_dir)
+        alpha2 = self.load_field('alpha.crosslinker', t_dir)
+
+        if not self.mesh_loaded:
+            self.load_mesh()
+
+        dv1 = alpha1 * self.V
+        dv2 = alpha2 * self.V
+
+        X1 = calc_val_weighted(self.C, dv1,
+                               normalised=True,
+                               fsave=join(o_dir, 'X.pregel.npy'))
+        X2 = calc_val_weighted(self.C, dv2,
+                               normalised=True,
+                               fsave=join(o_dir, 'X.crosslinker.npy'))
+
+    def calc_Ucm(self, time):
+        t_dir = join(self.case_dir, time)
+        o_dir = join(self.out_dir, time)
+        makedirs(o_dir, exist_ok=True)
+
+        if (exists(join(o_dir, 'U.pregel.npy')) and
+            exists(join(o_dir, 'U.crosslinker.npy'))):
+            return None
+
+        alpha1 = self.load_field('alpha.pregel', t_dir)
+        alpha2 = self.load_field('alpha.crosslinker', t_dir)
+        U = self.load_field('U', t_dir)
+        V1 = self.load_post_field('V.pregel.npy', time)
+        V2 = self.load_post_field('V.crosslinker.npy', time)
+
+        if not self.mesh_loaded:
+            self.load_mesh()
+
+        dv1 = alpha1 * self.V
+        dv2 = alpha2 * self.V
+
+        U1 = calc_val_weighted(U, dv1,
+                               normalised=True,
+                               fsave=join(o_dir, 'U.pregel.npy'))
+        U2 = calc_val_weighted(U, dv2,
+                               normalised=True,
+                               fsave=join(o_dir, 'U.crosslinker.npy'))
+        Ucm = (V1 * U1 + V2 * U2) / (V1 + V2)
+        save(join(o_dir, 'Ucm.npy'), Ucm)
+
+    def calc_impact_parameter(self, time):
+        t_dir = join(self.case_dir, time)
+        o_dir = join(self.out_dir, time)
+        makedirs(o_dir, exist_ok=True)
+
+        if exists(join(o_dir, 'impact_param.npy')):
+            return None
+
+        X1 = self.load_post_field('X.pregel.npy', time)
+        X2 = self.load_post_field('X.crosslinker.npy', time)
+
+        U1 = self.load_post_field('U.pregel.npy', time)
+        U2 = self.load_post_field('U.crosslinker.npy', time)
+
+        Ur = norm(U2 - U1)
+        b1 = norm(X2 - X1)
+        b2 = dot(U2 - U1, X2 - X1) / Ur
+        b = sqrt(b1 ** 2 - b2 ** 2)
+
+        B = 0.5 * b / self.Rnozzle
+        save(join(o_dir, 'impact_param.npy'), r_[b, B])
+
+    def calc_Reynolds(self, time):
+        t_dir = join(self.case_dir, time)
+        o_dir = join(self.out_dir, time)
+        makedirs(o_dir, exist_ok=True)
+
+        if exists(join(o_dir, 'Re_collision.npy')):
+            return None
+
+        U1 = self.load_post_field('U.pregel.npy', time)
+        U2 = self.load_post_field('U.crosslinker.npy', time)
+
+        Ur = norm(U2 - U1)
+        nu1 = self.transport_properties['pregel']['viscosity']
+        nu2 = self.transport_properties['crosslinker']['viscosity']
+
+        Re_collision = 4 * self.Rnozzle * Ur / (nu1 + nu2)
+        save(join(o_dir, 'Re_collision.npy'), Re_collision)
+
+    def calc_Weber(self, time):
+        t_dir = join(self.case_dir, time)
+        o_dir = join(self.out_dir, time)
+        makedirs(o_dir, exist_ok=True)
+
+        if exists(join(o_dir, 'We_collision.npy')):
+            return None
+
+        U1 = self.load_post_field('U.pregel.npy', time)
+        U2 = self.load_post_field('U.crosslinker.npy', time)
+
+        Ur = norm(U2 - U1)
+        rho1 = self.transport_properties['pregel']['density']
+        rho2 = self.transport_properties['crosslinker']['density']
+
+        We_collision = (rho1 + rho2) * self.Rnozzle * Ur * Ur / self.surface_tension
+        save(join(o_dir, 'We_collision.npy'), We_collision)
+
+    def calc_dSigma(self, time):
+        t_dir = join(self.case_dir, time)
+        o_dir = join(self.out_dir, time)
+        makedirs(o_dir, exist_ok=True)
+
+        if exists(join(o_dir, 'dSigma.npy')):
+            return None
+
+        alpha1     = self.load_field(     'alpha.crosslinker',  time)
+        alpha2     = self.load_field(          'alpha.pregel',  time)
+        gradAlpha1 = self.load_field('grad(alpha.crosslinker)', time)
+        gradAlpha2 = self.load_field(     'grad(alpha.pregel)', time)
+
+        if not self.mesh_loaded:
+            self.load_mesh()
+
+        dS = dSigma(alpha1, alpha2, gradAlpha1, gradAlpha2, self.V)
+        save(join(o_dir, 'dSigma.npy'), dS)
+
+    def calc_contact_area(self, time):
+        t_dir = join(self.case_dir, time)
+        o_dir = join(self.out_dir, time)
+        makedirs(o_dir, exist_ok=True)
+
+        if exists(join(o_dir, 'contact_surface_area.npy')):
+            return None
+
+        dS = self.load_post_field('dSigma.npy', time)
+        S = sum(norm(dS, axis=1))
+        save(join(o_dir, 'contact_surface_area.npy'), S)
+
+    def calc_volume_mixture(self, time):
+        t_dir = join(self.case_dir, time)
+        o_dir = join(self.out_dir, time)
+        makedirs(o_dir, exist_ok=True)
+
+        if exists(join(o_dir, 'mixtureVolume.npy')):
+            return None
+
+        alpha1 = self.load_field('alpha.pregel', t_dir)
+        alpha2 = self.load_field('alpha.crosslinker', t_dir)
+
+        if not self.mesh_loaded:
+            self.load_mesh()
+
+        dChi = 4.0 * alpha1 * alpha2 * self.V
+        save(join(o_dir, 'mixtureVolume.npy'), sum(dChi))
+
+
+    def calc_dissipation_rate(self, time):
+        t_dir = join(self.case_dir, time)
+        o_dir = join(self.out_dir, time)
+        makedirs(o_dir, exist_ok=True)
+
+        if exists(join(o_dir, 'scalarDissipationRate.npy')):
+            return None
+
+        alpha1 = self.load_field('alpha.pregel', t_dir)
+        alpha2 = self.load_field('alpha.crosslinker', t_dir)
+        magGradAlpha1 = norm(self.load_field('grad(alpha.pregel)', t_dir), axis=1)
+        magGradAlpha2 = norm(self.load_field('grad(alpha.crosslinker)', t_dir), axis=1)
+
+        if not self.mesh_loaded:
+            self.load_mesh()
+
+        dv1 = alpha1 * self.V
+        dv2 = alpha2 * self.V
+
+        E_mu = self.diffusivity * (dot(magGradAlpha2**2, dv1) +
+                                   dot(magGradAlpha1**2, dv2))
+        save(join(o_dir, 'scalarDissipationRate.npy'), E_mu)
+        return None
+
+    def calc_R(self, time):
+        t_dir = join(self.case_dir, time)
+        o_dir = join(self.out_dir, time)
+        makedirs(o_dir, exist_ok=True)
+
+        if exists(join(o_dir, '3rd_invariant.npy')):
+            return None
+
+        gradU = self.load_field('grad(U)', t_dir)
+        R = array(list(map(calc_3rd_inv, gradU)))
+        save(join(o_dir, '3rd_invariant.npy'), R)
+        return None
+
+    def calc_classification(self, time):
+        t_dir = join(self.case_dir, time)
+        o_dir = join(self.out_dir, time)
+        makedirs(o_dir, exist_ok=True)
+
+        if exists(join(o_dir, 'classification.npz')):
+            return None
+
+        Q = self.load_field('Q', t_dir)
+        R = self.load_post_field('3rd_invariant.npy', time)
+        n = 2 * (R > zeros_like(R)) + (4 * Q ** 3 + 27 * R ** 2 > zeros_like(Q))
+        savez_compressed(join(o_dir, 'classification.npz'), n)
+        return None
+
+    def calc_visc_dissipation_density(self, time):
+        t_dir = join(self.case_dir, time)
+        o_dir = join(self.out_dir, time)
+        makedirs(o_dir, exist_ok=True)
+
+        if exists(join(o_dir, 'visc_dissipation_density.npy')):
+            return None
+
         enstrophy = self.load_field('enstrophy', t_dir)
         Q = self.load_field('Q', t_dir)
         eps = 2.0 *(enstrophy - 2 * Q)
-        save(join(o_dir, 'visc_dissipation.npy'), eps)
+        save(join(o_dir, 'visc_dissipation_density.npy'), eps)
+        return None
+
+    def calc_eigensystem(self, time):
+        t_dir = join(self.case_dir, time)
+        o_dir = join(self.out_dir, time)
+        makedirs(o_dir, exist_ok=True)
+
+        if (exists(join(o_dir, 'eigenvector_1.npy')) and
+            exists(join(o_dir, 'eigenvector_2.npy')) and
+            exists(join(o_dir, 'eigenvector_3.npy')) and
+            exists(join(o_dir, 'eigenvalues.npy'))):
+            return None
+
+        gradU = self.load_field('grad(U)', time)
+        N = len(gradU)
+        W = zeros((N, 12))
+        for i in range(N):
+            W[i] = local_eigensystem(gradU[i])
+
+        save(join(o_dir, 'eigenvector_1.npy'), W[:,  0:3])
+        save(join(o_dir, 'eigenvector_2.npy'), W[:,  3:6])
+        save(join(o_dir, 'eigenvector_3.npy'), W[:,  6:9])
+        save(join(o_dir,   'eigenvalues.npy'), W[:, 9:12])
+        return None
 
     def calc_eigprojection(self, time):
         t_dir = join(self.case_dir, time)
         o_dir = join(self.out_dir, time)
         makedirs(o_dir, exist_ok=True)
 
-        alpha1 = self.load_field('alpha.crosslinker', time)
-        gradAlpha1 = self.load_field('grad(alpha.crosslinker)', time)
+        if (exists(join(o_dir, 'eigvec_1_projection.npy')) and
+            exists(join(o_dir, 'eigvec_3_projection.npy'))):
+            return None
 
-        alpha2 = self.load_field('alpha.pregel', time)
-        gradAlpha2 = self.load_field('grad(alpha.pregel)', time)
+        dS = self.load_post_field('dSigma.npy', time)
+        E1 = self.load_post_field('eigenvector_1.npy', time)
+        E3 = self.load_post_field('eigenvector_3.npy', time)
 
-        dsigma = (alpha1 * gradAlpha2 - alpha2 * gradAlpha1) * self.V
+        S = sum(norm(dS, axis=1))
+        save(join(o_dir, 'contact_surface_area.npy'), S)
+        pE1 = sum(abs(sum(E1 * dS, axis=1)))
+        pE3 = sum(abs(sum(E3 * dS, axis=1)))
 
-        evecs = self.load_post_field('eigenvectorsGradU.npy', time)
+        save(join(o_dir, 'eigvec_1_projection.npy'), pE1)
+        save(join(o_dir, 'eigvec_3_projection.npy'), pE3)
+        return pE1, pE3
 
+    def calc_topology_contact_surface(self, time):
+        t_dir = join(self.case_dir, time)
+        o_dir = join(self.out_dir, time)
+        makedirs(o_dir, exist_ok=True)
 
+        if exists(join(o_dir, 'surface_area_topology.npy')):
+            return None
 
-#
+        C = self.load_post_field('classification.npz', time)
+        dS = norm(self.load_post_field('dSigma.npy', time), axis=1)
 
-
-rc = readOFcase('/home/vvmv9/workspace/OFSims/mix_L1/ur-6')
-rc.set_nozzle_radius(2.5e-4)
-# rc.calc_dimensinless_numbers('0.001')
-# rc.calc_mixture_measures('0.001')
-# rc.calc_visc_dissipation('0.001')
-# rc.forAllTimes(rc.calc_R)
-# rc.forAllTimes(rc.calc_classification)
-# rc.forAllTimes(rc.calc_visc_dissipation)
-# rc.forAllTimes(rc.calc_mixture_measures)
-# rc.forAllTimes(rc.calc_dimensinless_numbers)
-
+        Cs = np.array([dot(C == i, dS) for i in range(4)])
+        save(join(o_dir, 'surface_area_topology.npy'), Cs)
+        return None
